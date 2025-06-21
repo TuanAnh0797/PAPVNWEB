0c81c4c534c11564e187b71ad55a6745febf7e70
TuanAnh <Tuananh00977@gmail.com>
2025-06-22 00:46:37 +0700

    2025-06-22-5

---
+++ PAPVN/Management/Management_logerror.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Management_logerror.aspx.cs" Inherits="PAPVN.Management.Management_logerror" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+      <div class="m-2">
+      <div class="row" style="background-color: #fffefe; border-radius: 8px;">
+          <div class="col" style="margin-top: 5px; margin-bottom: 5px">
+             <asp:FileUpload ID="FileUpload1" runat="server" Font-Size="20px" />
+          </div>
+          
+          <div class="col-sm-3" style="margin-top: 5px; margin-bottom: 5px">
+              <asp:Button type="button" Text="Upload" OnClick="btnImport_Click" runat="server" name="btn_Import" Style="font-size: 20px; font-weight: 600; padding: 5px" class="btn btn-outline-primary btn-block "></asp:Button>
+          </div>
+
+      </div>
+
+  </div>
+    <table style="padding-left: 5px; padding-right: 5px; padding-bottom: 10px; padding-top: 10px" id="dataTable" class="table table-striped table-bordered text-center">
+        <thead style="background-color: #b2bac2; color: black">
+            <tr class="p-0">
+                <th class="p-1">Name Machine </th>
+                <th class="p-1">Code Error</th>
+                <th class="p-1">Name Error </th>
+                <th class="p-1">Time Update </th>
+            </tr>
+        </thead>
+        <tbody id="tableBody" class="p-0" style="font-size: 14px" runat="server">
+        </tbody>
+    </table>
+    <script>
+        $(document).ready(function () {
+            // Khởi tạo DataTable
+            var table = $('#dataTable').DataTable({
+                pageLength: 3,
+                lengthChange: false,
+                paging: true,
+                searching: false,
+                ordering: false,
+                info: false,
+
+
+            });
+           
+        });
+    </script>
+</asp:Content>
---
+++ PAPVN/Management/Management_logerror.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySqlConnector;
+
+namespace PAPVN.Management
+{
+    public partial class Management_logerror : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loaddata();
+            }
+        }
+
+        protected void btnImport_Click(object sender, EventArgs e)
+        {
+            string ext = Path.GetExtension(FileUpload1.FileName).ToLower();
+            if (ext == ".xlsx")
+            {
+                //getting the path of the file 
+                string path = Server.MapPath("~/CodeError/" + FileUpload1.FileName);
+                //saving the file inside the MyFolder of the server
+                FileUpload1.SaveAs(path);
+                ReadExcelFile(path);
+                System.IO.File.Delete(path);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Thông báo", "alert('File upload phải có định dạng .xlsx')", true);
+            }
+        }
+       
+        private void ReadExcelFile(string filePath)
+        {
+            try
+            {
+                string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1;\"";
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
+                    // Lấy tên các sheet trong file Excel
+                    DataTable dtSheet = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    if (dtSheet != null)
+                    {
+                        string query = $"SELECT * FROM ErrorCode";
+                        using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            Getdata(dt);
+                        }
+                        //}
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Daily revise"))
+                {
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Thông báo", $"alert('File excel không tồn tại Sheet:ErrorCode')", true);
+                }
+            }
+        }
+        public void Getdata( DataTable dt)
+        {
+            // Đẩy dữ liệu lên server
+            DBConnect dBConnect = new DBConnect();
+            // Xóa kế hoạch cũ
+            dBConnect.exnonquery("TA_sp_ClearAllErorCode", CommandType.StoredProcedure);
+            // Đẩy kế hoạch mới
+            SaveMySql(dt);
+            // tải lại kế hoạch mới
+            loaddata();
+        }
+        public void SaveMySql(DataTable data)
+        {
+            using (MySqlConnector.MySqlConnection connection = new MySqlConnector.MySqlConnection(DBConnect.connection_string))
+            {
+                connection.Open();
+
+                try
+                {
+                    var bulkCopy = new MySqlBulkCopy(connection);
+                    bulkCopy.DestinationTableName = "codeerror";
+                    var result = bulkCopy.WriteToServer(data);
+                  
+                }
+                catch (Exception ex)
+                {
+
+                }
+                connection.Close();
+            }
+        }
+
+        private void loaddata()
+        {
+            string HTML = "";
+            DBConnect dBConnect = new DBConnect();
+            DataTable dt = dBConnect.StoreFillDT("TA_sp_LoadDataError", CommandType.StoredProcedure);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+              
+                    
+                        HTML += $"<tr> " +
+                        $"<td> {dt.Rows[i]["NameMachine"].ToString()} </td>" +
+                        $"<td> {dt.Rows[i]["CodeError"].ToString()} </td>" +
+                        $"<td> {dt.Rows[i]["NameError"].ToString()} </td>" +
+                        $"<td> {dt.Rows[i]["TimeUpdate"].ToString()} </td>" +
+                        $"</tr>";
+                   
+                  
+              
+            }
+            tableBody.InnerHtml = HTML;
+        }
+    }
+}
